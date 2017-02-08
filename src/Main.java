6696c8ab3207c5a51a88286700813e600ee12f82
Anatolijus <join.eu.pn@gmail.com>
2017-02-08 17:01:51 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = -100; i < 101; i++) {
+            if (i != 0 && i%3 == 0) System.out.print("Fizz");
+            if (i != 0 && i%5 == 0) System.out.print("Buzz");
+            if (i == 0 || i%3 != 0 && i%5 != 0) System.out.print(i);
+            System.out.print("\n");
+        }
+    }
+}
